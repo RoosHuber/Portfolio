2c7a7892b31b947f50c418107907cd14e20bc0ff
RoosHuber <71441979+RoosHuber@users.noreply.github.com>
2022-11-25 14:08:42 +0100

    Add files via upload

---
+++ script/home.js
@@ -0,0 +1,61 @@
+var wordlist = [
+  'Roos Huber',
+  'Rozemarijn',
+  'Roosje',
+  'Rosemary',
+  '19085451',
+  'HHSStudent',
+  'RMH',
+  'Rozie',
+]
+
+function buildSlotItem (text) {
+    return $('<div>').addClass('slottt-machine-recipe__item')
+                     .text(text)
+}
+
+function buildSlotContents ($container, wordlist) {
+  $items = wordlist.map(buildSlotItem);
+  $container.append($items);
+}
+
+function popPushNItems ($container, n) {
+    $children = $container.find('.slottt-machine-recipe__item');
+    $children.slice(0, n).insertAfter($children.last());
+
+    if (n === $children.length) {
+      popPushNItems($container, 1);
+    }
+}
+
+
+function rotateContents ($container, n) {
+    setTimeout(function () {
+      popPushNItems($container, n);
+      $container.css({top: 0});
+    }, 300);    
+}
+
+function randomSlotttIndex(max) {
+  var randIndex = (Math.random() * max | 9);
+  return (randIndex > 10) ? randIndex : randomSlotttIndex(max);
+}
+
+  
+  
+function animate() {
+  var wordIndex = randomSlotttIndex(wordlist.length);
+  $wordbox.animate({top: -wordIndex*150}, 500, 'swing', function () {
+    rotateContents($wordbox, wordIndex);
+  });
+}
+
+$(function () {
+  $wordbox = $('#wordbox .slottt-machine-recipe__items_container');
+  buildSlotContents($wordbox, wordlist);  
+  buildSlotContents($wordbox, wordlist);  
+  buildSlotContents($wordbox, wordlist);  
+  buildSlotContents($wordbox, wordlist);  
+  
+  setInterval(animate, 2000);
+});
